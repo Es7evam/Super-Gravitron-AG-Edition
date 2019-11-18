a7df200038059d3fa948e6666ff05b5de537a889
Es7evam <estevam.arantes@gmail.com>
2019-11-18 15:13:00 -0300

    Add debug function at main file

--- gravitron.cpp
+++ gravitron.cpp
@@ -9,8 +9,15 @@
 
 // Own codes
 #include "classes/Window.hh"
-
 #include <stdio.h>
+
+void debug(std::vector<Blast> B){
+    int i=0;
+    std::cerr << "Debug: " << B.size() << "\n";
+    for (auto singleBlast : B){
+        std::cerr  << i++  << "  " << singleBlast.x() << " " << singleBlast.y() << " -> " << singleBlast.leftright() << std::endl; 
+    }
+}
 
 int main(){
 
@@ -24,22 +31,32 @@ int main(){
     clear(); // limpa tela
 
     Player P;
+    std::vector<Blast> B;
     std::pair <int, int> coord;
 
     Window W;
 
     int key;
+    int cont = 0;
     while(true){
         //printf("%d %d\n", P.x, P.y);
         //sleep(1);
         coord = std::make_pair(P.x(), P.y());
         P.Update();
+        for(long unsigned int i=0;i<B.size();i++){
+            B[i].Update();
+        }
         key = getch();
         if(key < 10000 && key != -1)
             P.Change_dir(key);
 
         // Renders Window
-        W.Update(P, coord);
+        W.Update(P, &B, coord);
+        
+        if(cont++ == 100){
+            //debug(B);
+            cont=0;
+        }
     }
 
     return 0;
